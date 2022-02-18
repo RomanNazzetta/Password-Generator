24eea449308f34752bd3cd790bcca0fa23869c9e
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-18 11:52:34 -0300

    sorting fruits

--- test.js
+++ test.js
@@ -54,10 +54,28 @@
 
 //ROCK PAPER SCISSOR
 
-let hands = ["rock", "paper","scissor"]
+// let hands = ["rock", "paper","scissor"]
 
-function getHand(){
-    let randomIndex = Math.floor( Math.random() * 3)
-    return hands[randomIndex]
+// function getHand(){
+//     let randomIndex = Math.floor( Math.random() * 3)
+//     return hands[randomIndex]
+// }
+// console.log(getHand())
+
+//SORTING FRUITS
+
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+function sortFruit(){
+    for (let i = 0; i< fruit.length; i++){
+        if (fruit[i] === "🍎"){
+            appleShelf.textContent += "🍎"
+        } else if (fruit[i] === "🍊" ){
+            orangeShelf.textContent += "🍊"
+        }
+    }
 }
-console.log(getHand())
+
+sortFruit()
