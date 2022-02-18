11155fd7c89cef98f6aeff50ec964f5b5b0e0114
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-18 16:24:11 -0300

    functions setup

--- test.js
+++ test.js
@@ -64,18 +64,18 @@
 
 //SORTING FRUITS
 
-let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
-let appleShelf = document.getElementById("apple-shelf")
-let orangeShelf = document.getElementById("orange-shelf")
+// let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+// let appleShelf = document.getElementById("apple-shelf")
+// let orangeShelf = document.getElementById("orange-shelf")
 
-function sortFruit(){
-    for (let i = 0; i< fruit.length; i++){
-        if (fruit[i] === "🍎"){
-            appleShelf.textContent += "🍎"
-        } else if (fruit[i] === "🍊" ){
-            orangeShelf.textContent += "🍊"
-        }
-    }
-}
+// function sortFruit(){
+//     for (let i = 0; i< fruit.length; i++){
+//         if (fruit[i] === "🍎"){
+//             appleShelf.textContent += "🍎"
+//         } else if (fruit[i] === "🍊" ){
+//             orangeShelf.textContent += "🍊"
+//         }
+//     }
+// }
 
-sortFruit()
+// sortFruit()
