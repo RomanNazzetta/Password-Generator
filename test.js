3d37a8a71ed5dacf18a67774ba63ce60ece4c306
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-18 11:00:42 -0300

    passangers disc

--- test.js
+++ test.js
@@ -1,11 +1,26 @@
-let person = {
-    age: 35,
-    name: "Roman",
-    country: "Argentina"
-}
+// let person = {
+//     age: 35,
+//     name: "Roman",
+//     country: "Argentina"
+// }
 
-function logData(){
-    console.log(person.name + " is "+ person.age+" years old and lives in " + person.country)
-}
+// function logData(){
+//     console.log(person.name + " is "+ person.age+" years old and lives in " + person.country)
+// }
 
-logData()
+// logData()
+
+let age = 5
+
+
+    if(age < 6){
+        console.log("Free")
+    } else if( age < 18){
+        console.log ("Child Discount")
+    } else if(age < 27){
+        console.log("Student Discount")
+    }else if(age < 67){
+        console.log("Full Price")
+    }else {
+        console.log("Senior Citizen Discount")
+    }
