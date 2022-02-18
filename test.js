2d8d53dc97cb8008c3fde612de7a9cacd1eb5310
RomanNazzetta <nazzetta.roman@gmail.com>
2022-02-18 11:12:12 -0300

    for loop

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+//OBJECT CREATION
 // let person = {
 //     age: 35,
 //     name: "Roman",
@@ -10,17 +11,24 @@
 
 // logData()
 
-let age = 5
+// let age = 5
 
+//IF ELSE STATEMENT
+//     if(age < 6){
+//         console.log("Free")
+//     } else if( age < 18){
+//         console.log ("Child Discount")
+//     } else if(age < 27){
+//         console.log("Student Discount")
+//     }else if(age < 67){
+//         console.log("Full Price")
+//     }else {
+//         console.log("Senior Citizen Discount")
+//     }
 
-    if(age < 6){
-        console.log("Free")
-    } else if( age < 18){
-        console.log ("Child Discount")
-    } else if(age < 27){
-        console.log("Student Discount")
-    }else if(age < 67){
-        console.log("Full Price")
-    }else {
-        console.log("Senior Citizen Discount")
-    }
+// FOR LOOP
+// let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+// console.log("The 5 largest countries in the world: ")
+// for (let i = 0; i < largeCountries.length; i++){
+//     console.log("- "+largeCountries[i])
+// }
